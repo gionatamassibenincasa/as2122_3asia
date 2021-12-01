918b611b2f2d5079b5b36a46852af08c948d22d6
Gionata Massi <gionata.massi@savoiabenincasa.it>
2021-12-01 13:46:46 +0100

    Modificato esempio per includere valore intero

--- 2021-12-01/tipo_float.cpp
+++ 2021-12-01/tipo_float.cpp
@@ -15,12 +15,16 @@ using namespace std;
 int main()
 {
     float f = 1.23456789;
+    float fc = 299792458;
     double d = -1.23456789012345;
+    float dc = 299792458;
     long double ld = -1.23456789012345E300;
 
     cout
         << "f (float): " << f << endl
+        << "fc (float): " << fc << " atteso 299792458" << endl
         << "d (double): " << d << endl
+        << "dc (double): " << dc << " atteso 299792458" << endl
         << "ld (long double): " << ld << endl;
 
     cout << "Un float occupa " << sizeof(float) << " byte" << endl;
