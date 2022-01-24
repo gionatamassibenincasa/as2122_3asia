94ded6b190184f28c135aebd2dd6e974dda622c4
Gionata Massi (in qualità di docente d'Informatica) <gionata.massi@savoiabenincasa.it>
2022-01-24 11:48:17 +0000

    Create soluzioni degli esercizi 1--4 del compito

---
+++ 2022-01-24/build/CMakeFiles/01_ridotto.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/01_ridotto.dir/ridotto.cpp.o"
+  "01_ridotto.pdb"
+  "01_ridotto"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_ridotto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
