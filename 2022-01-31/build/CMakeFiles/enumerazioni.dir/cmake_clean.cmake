c61181c3013b6b84cd8b15e2d44b6804ffe3046f
Gionata Massi (in qualità di docente d'Informatica) <gionata.massi@savoiabenincasa.it>
2022-01-31 12:39:19 +0000

    Creati esempi di passaggio di parametri, overloading di funzioni e enumerazioni

---
+++ 2022-01-31/build/CMakeFiles/enumerazioni.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/enumerazioni.dir/enumerazioni.cpp.o"
+  "enumerazioni.pdb"
+  "enumerazioni"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/enumerazioni.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2022-01-31/build/CMakeFiles/passaggio_parametri.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/passaggio_parametri.dir/passaggio_parametri.cpp.o"
+  "passaggio_parametri.pdb"
+  "passaggio_parametri"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/passaggio_parametri.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
