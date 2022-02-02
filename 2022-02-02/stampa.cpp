559ce07148c63f37f5c24b94ea5956a0a9337ffe
Gionata Massi (in qualità di docente d'Informatica) <gionata.massi@savoiabenincasa.it>
2022-02-02 12:39:45 +0000

    Aggiunto un array di 100 elementi per provare la stampa

--- 2022-02-02/stampa.cpp
+++ 2022-02-02/stampa.cpp
@@ -13,5 +13,10 @@ void stampa_array(float A[], int len) {
 int main() {
   float voti[5] = {6, 9, 3, 3, 9};
   stampa_array(voti, sizeof(voti) / sizeof(float));
+  // a è array di 100 float, a[i] = i
+  float a[100];
+  for(int i = 0; i < 100; i++)
+    a[i] = i;
+  stampa_array(a, sizeof(a) / sizeof(float));
   return 0;
 }
