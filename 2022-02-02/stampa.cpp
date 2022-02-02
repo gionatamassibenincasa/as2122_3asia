ae2a9dfe982200aeaaa3538f1954a0ac88914133
Gionata Massi (in qualità di docente d'Informatica) <gionata.massi@savoiabenincasa.it>
2022-02-02 11:57:35 +0000

    Creato primo esempio di array

---
+++ 2022-02-02/stampa.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float voti[5] = {6, 9, 3, 3, 9};
+  cout << "{" << voti[0];
+  cout << ", " << voti[1];
+  cout << ", " << voti[2];
+  cout << ", " << voti[3];
+  cout << ", " << voti[4];
+  cout << "}" << endl;
+
+  return 0;
+}
